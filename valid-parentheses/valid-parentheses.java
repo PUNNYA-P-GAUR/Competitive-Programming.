a4f2667f67a9b6157dfe1e4bf6fad46bfd098dd9
Punnya Prasoon Gaur <gaurpunnya.02@gmail.com>
2023-06-20 22:23:47 +0530

    Time: 2 ms (91.48%) | Memory: 40.4 MB (94.95%) - LeetSync

---
+++ valid-parentheses/valid-parentheses.java
@@ -0,0 +1,24 @@
+class Solution {
+    public boolean isValid(String s) {
+        Stack<Character> st=new Stack<>();
+        for(int i=0;i<s.length();i++){
+            Character ch=s.charAt(i);
+            if(ch=='('){
+                st.push(')');
+            }
+            else if(ch=='{'){
+                st.push('}');
+
+            }
+            else if(ch=='['){
+                st.push(']');
+            }
+            else{
+                if(st.isEmpty()||st.pop()!=ch){
+                    return false;
+                }
+            }
+        }
+        return st.isEmpty();
+    }
+}
