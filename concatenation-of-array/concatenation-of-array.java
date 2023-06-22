5bcf6a18731475690a0eb7e54e18bb83750c379b
Punnya Prasoon Gaur <gaurpunnya.02@gmail.com>
2023-06-23 00:09:31 +0530

    Time: 1 ms (93.78%) | Memory: 44.2 MB (50.74%) - LeetSync

--- concatenation-of-array/concatenation-of-array.java
+++ concatenation-of-array/concatenation-of-array.java
@@ -1,23 +1,20 @@
 class Solution {
     public int[] getConcatenation(int[] nums) {
+        int a=nums.length;
         int[] arr=nums;
-        // for(int i=0;i<nums.length;i++){
-        //     nums[i]=arr[i];
-
-        // }
-        int size=arr.length+nums.length;
+        int b=arr.length;
+        int c=a+b;
+        int[] res=new int[c];
         int pos=0;
-        int[] res=new int[size];
         for(int ele:nums){
-            
             res[pos]=ele;
             pos++;
         }
         for(int ele:arr){
-            //int pos=0;
             res[pos]=ele;
             pos++;
         }
         return res;
+
     }
 }
