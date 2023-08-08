4282639aa17be0749ca2a22eca26ccb7bad6e8b7
Punnya Prasoon Gaur <gaurpunnya.02@gmail.com>
2023-08-08 12:01:55 +0530

    Time: 5 ms (31.46%) | Memory: 8 MB (87.06%) - LeetSync

---
+++ 1319-unique-number-of-occurrences/unique-number-of-occurrences.cpp
@@ -0,0 +1,43 @@
+class Solution {
+public:
+   bool uniqueOccurrences(vector<int>& arr) {
+    
+    vector<int> index;
+
+    // sorting the original array
+    sort(arr.begin(), arr.end());
+    
+    
+    int c=0;
+    int count=1;
+    
+    // running loop for checking the array
+    for(int i=1;i<arr.size();i++)
+    {
+        if(arr[i]==arr[i-1])
+            count++;
+        
+        else
+           { 
+        index.push_back(count);
+        c++;
+            count=1;
+        }  
+    }
+    
+    //pushing last element
+    index.push_back(count);
+    
+    
+    sort(index.begin(), index.end());
+    
+    for(int i=1;i<=c;i++)
+    {
+        if(index[i]==index[i-1])
+            return false;
+    }
+    
+    return true;
+    
+}
+};
